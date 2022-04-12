102fa3377a2e6b0cee0b2875ddb68e966c8e968c
jokerotis <63875651+jokerotis@users.noreply.github.com>
2022-04-12 12:36:45 +0100

    #3: Hello Person Controller (second basic test)

--- src/main/java/com/otisps/demo/PersonController.java
+++ src/main/java/com/otisps/demo/PersonController.java
@@ -2,13 +2,15 @@ package com.otisps.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PersonController {
-
+    
     @GetMapping("/hello")
-    public String getHelloWorld(){
-        return "hello world!";
+    public String HelloPerson(@RequestBody Person person){
+        String message = "Hello " + person.givenName + " " + person.getLastName() + " !";
+        return message;
     }
 }
